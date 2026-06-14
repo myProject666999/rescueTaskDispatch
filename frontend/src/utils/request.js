dd2e2d1fa5f77dcdd79fc89f89b6023cee561764
luke <luke@example.com>
2026-06-14 19:38:47 +0800

    2

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
 const request = axios.create({
-  baseURL: '/api',
+  baseURL: 'http://localhost:8080/api',
   timeout: 15000,
 });
+
+request.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 request.interceptors.response.use(
   (response) => {
